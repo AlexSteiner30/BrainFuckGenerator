85bb98058c193fcc24f3a903ccd3bec671e796d9
AlexSteiner30 <65250433+AlexSteiner30@users.noreply.github.com>
2022-12-14 21:31:10 +0100

    Run C++ code

--- GUI/Main.java
+++ GUI/Main.java
@@ -1,30 +1,30 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.*;
- 
+import java.io.FileWriter;
+
 public class Main implements ActionListener {
-    public static void main(String[] args){
-        Main window = new Main();
+
+    // Create Components
+    private static JLabel label = new JLabel("Write here you code in C++");
+    private static JTextArea input = new JTextArea();
+    private static JButton button = new JButton("Convert");
+
+    public static void main(String[] args) {
+        new Main();
     }
 
     // Window Builder
-    Main()
-    {
+    Main() {
         // Create Frane
         JFrame frame = new JFrame();
 
-        // Create Components
-        JLabel label = new JLabel("Write here you code in C++");
-        JTextArea input = new JTextArea();  
-        JButton button =new JButton("Convert");  
-        
         // Button
         button.addActionListener(this);
 
         // Set Bounds
-        label.setBounds(26,25, 200,30);  
-        input.setBounds(25,55, 400,350);  
-        button.setBounds(20,415, 100,30);  
+        label.setBounds(26, 25, 200, 30);
+        input.setBounds(25, 55, 400, 350);
+        button.setBounds(20, 415, 100, 30);
 
         // Add Componnets
         frame.add(label);
@@ -32,21 +32,31 @@ public class Main implements ActionListener {
         frame.add(button);
 
         // Run window
-        frame.setSize(500,500);
-        frame.setResizable(false); 
+        frame.setSize(500, 500);
+        frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("BrainFuck GUI Generator");
-        frame.setLayout(null);  
+        frame.setLayout(null);
         frame.setVisible(true);
     }
 
     public void actionPerformed(ActionEvent e) {
         CompileCode();
-    } 
+    }
 
     // Functions
-    private static void CompileCode()
-    {
-        
+    private static void CompileCode() {
+        try {
+            FileWriter cppFile = new FileWriter("code.cpp");
+            cppFile.write(input.getText());
+            cppFile.close();
+
+            Process code = Runtime.getRuntime().exec("g++ code.cpp -o function");
+            code.waitFor();
+        }
+
+        catch (Exception e) {
+            System.out.println("An error occurred: " + e.getLocalizedMessage());
+        }
     }
 }
---
+++ GUI/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo test
+g++ code.cpp
+clear
+./a.out
