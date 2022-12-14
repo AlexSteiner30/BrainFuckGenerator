800f7dac8534a852e90560b9f9da3df286441964
AlexSteiner30 <65250433+AlexSteiner30@users.noreply.github.com>
2022-12-14 19:28:24 +0100

    GUI

--- GUI/Main.java
+++ GUI/Main.java
@@ -1,38 +1,52 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
-public class Main {
-    public static void main(String[] args) {
-        printString("a");
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+ 
+public class Main implements ActionListener {
+    public static void main(String[] args){
+        Main window = new Main();
     }
 
-    private static void printString(String input) {
-        String startingString = new String();
-        int count = 0;
+    // Window Builder
+    Main()
+    {
+        // Create Frane
+        JFrame frame = new JFrame();
 
-        for(int i = 0; i < input.length(); i++)
-        {
-            for(int j = 0; j < (int)input.charAt(i); j++)
-            {
-                startingString += "+";
-            }
+        // Create Components
+        JLabel label = new JLabel("Write here you code in C++");
+        JTextArea input = new JTextArea();  
+        JButton button =new JButton("Convert");  
+        
+        // Button
+        button.addActionListener(this);
 
-            count = (int)input.charAt(i);
-            startingString += ".>";
-        }
+        // Set Bounds
+        label.setBounds(26,25, 200,30);  
+        input.setBounds(25,55, 400,350);  
+        button.setBounds(20,415, 100,30);  
 
-        try
-        {
-            FileWriter brainFuckFile = new FileWriter("brainfuck.bf");
-            brainFuckFile.write(startingString);
-            brainFuckFile.close();
+        // Add Componnets
+        frame.add(label);
+        frame.add(input);
+        frame.add(button);
 
-            Runtime.getRuntime().exec("brainfuck brainfuck.bf");
-        }
+        // Run window
+        frame.setSize(500,500);
+        frame.setResizable(false); 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("BrainFuck GUI Generator");
+        frame.setLayout(null);  
+        frame.setVisible(true);
+    }
 
-        catch(Exception e) {
-            // Error
-        }
+    public void actionPerformed(ActionEvent e) {
+        CompileCode();
+    } 
+
+    // Functions
+    private static void CompileCode()
+    {
+        
     }
 }
